948770fa9e630db5b2c5b679e5a2556e1597ff4f
huongdoan-hugo <huongdd99@gmail.com>
2021-12-27 18:01:56 +0700

    update 2712

---
+++ javascrip.js
@@ -0,0 +1,10 @@
+// const buyBtns = document.querySelectorAll('.js-buy-ticket')
+// const modal = document.querySelector('.modal')
+
+// function showBuyTickets() {
+//     alert('oki')
+// }
+
+// for (const buyBtn of buyBtns) {
+//     buyBtn.addEventListener('click', showBuyTickets)
+// }
